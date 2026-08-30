5be05414202323c979e3e9a615d16528ddf2a960
chloemarieconnolly <chloe.marie.connolly@gmail.com>
2026-08-30 22:39:05 +0000

    UI design p2

--- script.js
+++ script.js
@@ -5,24 +5,20 @@ fetch(`https://api.github.com/users/${username}/starred`)
   .then(repos => {
     const list = document.querySelector("#stargazers-list");
     
-    repos.forEach(repo => {
-      const item = document.createElement("li");
-      
-      const description = repo.description || "No description";
-      const shortDescription = description.length > 100 
+    const filteredRepos = repos.filter(repo => repo.owner.login !== username);
+
+filteredRepos.forEach(repo => {
+    const item = document.createElement("li");
+    
+    const description = repo.description || "No description";
+    const shortDescription = description.length > 100 
         ? description.slice(0, 100) + "..." 
         : description;
 
       item.innerHTML = `
-        <span class="repo-name">${repo.full_name}</span>
-        <span class="repo-desc">${shortDescription}</span>
-      `;
-
-      item.addEventListener("click", () => {
-        const desc = item.querySelector(".repo-desc");
-        const isHidden = window.getComputedStyle(desc).display === "none";
-        desc.style.display = isHidden ? "block" : "none";
-      });
+  <a href="${repo.html_url}" target="_blank" class="repo-name">${repo.full_name}</a>
+  <span class="repo-desc">${shortDescription}</span>
+`;
 
       list.appendChild(item);
     });
